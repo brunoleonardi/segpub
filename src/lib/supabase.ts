1410b78caa4f09c661849e4c3f834b4ac4ed8eeb
brunoleonardi <bruno_m_leonardi@hotmail.com>
2025-05-29 01:20:18 -0300

    feat: some changes on components interface

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,4 +4,4 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 // Create a single supabase client for interacting with your database
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey); 
