b0407326108434415a8d8d682614300e1336c254
Bruno Leonardi <82687833+brunoleonardi@users.noreply.github.com>
2025-05-29 00:01:47 -0300

    Updated ControlTablePage.tsx

--- src/screens/ControlTablePage/ControlTablePage.tsx
+++ src/screens/ControlTablePage/ControlTablePage.tsx
@@ -10,18 +10,7 @@ export const ControlTablePage = () => {
 
   return (
     <div className={`min-h-screen ${isDarkMode ? 'bg-zinc-900' : 'bg-[#EFF4FA]'}`}>
-      <div className="p-6">
-        {/* <button 
-          onClick={() => navigate('/')}
-          className={`mb-6 flex items-center gap-2 px-4 py-2 rounded-lg transition-colors ${
-            isDarkMode 
-              ? 'bg-zinc-800 text-gray-300 hover:bg-zinc-700' 
-          }`}
-        > */}
-          <ArrowLeft size={20} />
-          <span>Voltar</span>
-        </button>
-      </div>
+
       <ControlTable isDarkMode={isDarkMode} title={section || ''} />
     </div>
   );
