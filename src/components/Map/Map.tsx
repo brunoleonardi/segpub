bbf8a43826fb42e3bc6f3f20c2dbfe1dedd1ecda
Bruno Leonardi <82687833+brunoleonardi@users.noreply.github.com>
2025-05-29 14:23:08 -0300

    Updated Map.tsx

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -147,7 +147,7 @@ export const MapComponent: React.FC<MapComponentProps> = ({ isDarkMode }) => {
       getIcon: d => 'marker',
       sizeScale: 2,
       getPosition: d => [d.longitude, d.latitude],
-      getSize: d => 24,
+      getSize: d => 5,
       getColor: d => {
         const color = d.type_color.match(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/)?.[0] || '#000000';
         const r = parseInt(color.slice(1, 3), 16);
