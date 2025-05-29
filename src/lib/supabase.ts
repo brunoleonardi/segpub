f0a261eaa9e0ab5a9fa55d2bc14f10afb4d04af4
Bruno Leonardi <82687833+brunoleonardi@users.noreply.github.com>
2025-05-28 21:17:19 -0300

    Fix Supabase client initialization

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://oeszfegjwaauflelgxse.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9lc3pmZWdqd2FhdWZsZWxneHNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0NzcwNjIsImV4cCI6MjA2NDA1MzA2Mn0.8QCsKap4r7jQ6bWt1b8hhTyjt3qn4bdyO5rxli_iiXE';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
