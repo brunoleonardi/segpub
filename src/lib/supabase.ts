90c3f9f12ebe7b862b398f17dbe61c2162fa72d9
Bruno Leonardi <82687833+brunoleonardi@users.noreply.github.com>
2025-05-28 21:16:46 -0300

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://oeszfegjwaauflelgxse.supabase.co';
-const supabaseKey = 'YOUR_SUPABASE_KEY';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9lc3pmZWdqd2FhdWZsZWxneHNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0NzcwNjIsImV4cCI6MjA2NDA1MzA2Mn0.8QCsKap4r7jQ6bWt1b8hhTyjt3qn4bdyO5rxli_iiXE';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
